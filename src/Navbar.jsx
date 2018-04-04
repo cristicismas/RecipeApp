0d1a33cef251c3b24b1724ae85269e750d5cb2df
Cristi <cristicismas99@gmail.com>
2018-04-04 11:42:30 +0300

    Add navbar info to recipes number

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -10,7 +10,7 @@ class Navbar extends Component {
 			<header>
 				<h1>RecipeApp</h1>
 				{recipesLeft > 0
-					? <div className="wrapper"><h4>Recipes left: <span id="recipes-left-number">{recipesLeft}</span></h4></div>
+					? <div className="wrapper"><h4>Recipes left: <span id="recipes-left-number" title="Recipes are limited because browsers limit local storage.">{recipesLeft}</span></h4></div>
 					: <div className="wrapper"><h3 id="no-recipes-left">NO RECIPES LEFT</h3></div>}
 				<a onClick={this.props.onNewRecipe}><h3>New Recipe</h3></a>
 			</header>
